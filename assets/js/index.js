26ebc8237af9a04d86e2eaa797a5048ad5730888
Som&soft <som8soft@gmail.com>
2021-11-05 22:06:18 +0200

    Final version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,24 @@
 'use strict';
 
+fetch('/assets/js/fruits.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const allFruits = [];
+    const redFruits = [];
+    const largeFruits = [];
+    data.forEach(({ fruit, size, color }) => {
+      allFruits.push(fruit);
+      if (color === 'Red') {
+        redFruits.push(fruit);
+      }
+      if (size === 'Large') {
+        largeFruits.push(fruit);
+      }
+    });
+    console.log('All fruits:', allFruits.join(', '));
+    console.log('Red fruits:', redFruits.join(', '));
+    console.log('Large fruits:', largeFruits.join(', '));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
